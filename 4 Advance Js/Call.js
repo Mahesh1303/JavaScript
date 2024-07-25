21a4b567e8df2411e17bcbd4d87be9bd4b48373a
Mahesh Divate <157132047+Mahesh1303@users.noreply.github.com>
2024-07-25 22:32:34 +0530

    Update Call.js

--- 4 Advance Js/Call.js
+++ 4 Advance Js/Call.js
@@ -4,7 +4,8 @@ function Calluser(username){
 function userData(username,id,password){
     this.id=id;
     this.password=password
-    Calluser.call(this,username)//call is used for refrencing the user and this is used to refrence the conteext of the function called
+    Calluser.call(this,username)//call is used for refrencing the function called  and this is used to refrence the conteext of the function called
+    // that has been called
 }
 
 const person= new userData("mahesh",21,"xyzzz")
